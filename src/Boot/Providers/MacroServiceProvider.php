928107cf480b269d6e76d085b7d8eb7e71fec5b4
Hilkiah Makemo <hilkiahmakemo@planstin.com>
2020-09-30 12:11:05 -0600

    added macro service provider:

---
+++ src/Boot/Providers/MacroServiceProvider.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace Core\Boot\Providers;
+use Illuminate\Support\Collection;
+use Illuminate\Pagination\LengthAwarePaginator;
+
+class MacroServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Collection::macro('paginate', function($perPage, $total = null, $page = null, $pageName = 'page') {
+            $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+
+            return new LengthAwarePaginator(
+                $this->forPage($page, $perPage),
+                $total ?: $this->count(),
+                $perPage,
+                $page,
+                [
+                    'path' => LengthAwarePaginator::resolveCurrentPath(),
+                    'pageName' => $pageName,
+                ]
+            );
+        });
+    }
+
+}
---
+++ src/Boot/Services/Pagination.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace Core\Boot;
+
+
+class Pagination
+{
+
+}
