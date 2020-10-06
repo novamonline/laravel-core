ec7b28a46b4fbb1ac36ce37a3ebef0d1415fd818
Hilkiah Makemo <hilkiahmakemo@planstin.com>
2020-10-05 22:40:45 -0600

    fixed paginate for both eloquent and support collections

--- src/Boot/Providers/MacroServiceProvider.php
+++ src/Boot/Providers/MacroServiceProvider.php
@@ -5,7 +5,8 @@ namespace Core\Boot\Providers;
 
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Pagination\Paginator;
-use Illuminate\Support\Collection;
+use Illuminate\Support\Collection as SupportCollection;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 use Illuminate\Support\ServiceProvider;
 
 class MacroServiceProvider extends ServiceProvider
@@ -28,22 +29,34 @@ class MacroServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-        if (!Collection::hasMacro('paginate')) {
+        if (!SupportCollection::hasMacro('paginate')) {
 
-            Collection::macro('paginate', function($perPage, $total = null, $page = null, $pageName = 'page') {
-                $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
-
-                return new LengthAwarePaginator(
-                    $this->forPage($page, $perPage),
-                    $total ?: $this->count(),
-                    $perPage,
-                    $page,
-                    [
-                        'path' => LengthAwarePaginator::resolveCurrentPath(),
-                        'pageName' => $pageName,
-                    ]
-                );
+            SupportCollection::macro('paginate', function($perPage, $total = null, $page = null, $pageName = 'page') {
+                return $this->paginate($perPage, $total, $page, $pageName);
             });
         }
+
+        if (!EloquentCollection::hasMacro('paginate')) {
+
+            EloquentCollection::macro('paginate', function($perPage, $total = null, $page = null, $pageName = 'page') {
+                return $this->paginate($perPage, $total, $page, $pageName);
+            });
+        }
+    }
+
+    public function paginate($perPage, $total = null, $page = null, $pageName = 'page')
+    {
+        $page = $page ?: LengthAwarePaginator::resolveCurrentPage($pageName);
+
+        return new LengthAwarePaginator(
+            $this->forPage($page, $perPage),
+            $total ?: $this->count(),
+            $perPage,
+            $page,
+            [
+                'path' => LengthAwarePaginator::resolveCurrentPath(),
+                'pageName' => $pageName,
+            ]
+        );
     }
 }
