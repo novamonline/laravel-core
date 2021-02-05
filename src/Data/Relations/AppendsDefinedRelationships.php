84ecea11cf55a9452aa9b4d23a3f5ee23622d012
Hilkiah Makemo <hilkiahmakemo@planstin.com>
2021-02-04 21:45:36 -0700

    updated appended relationships

--- src/Data/Relations/AppendsDefinedRelationships.php
+++ src/Data/Relations/AppendsDefinedRelationships.php
@@ -12,14 +12,24 @@ trait AppendsDefinedRelationships
 {
     public function getRelationshipsAttribute()
     {
-        $relationships = new Collection([]);
+        $relationships = new Collection();
 
         $resource = $this->resource ?: $this->toArray();
 
         foreach ($resource as $field => $value) {
             if (Str::contains($field, '_id')) {
-                $name = (string)Str::of($field)->before('_id')->camel()->ucfirst();
-                $relationships->$name = $resource->$name ?? null;
+
+                $name = (string)Str::of($field)->before('_id')->camel()->lower();
+
+                $relation = $this->$name ?? null;
+                if($relation){
+                    $relationships->$name = $relation ?? [];
+                }
+
+                $relations = $this->{$name = Str::plural($name)} ?? null;
+                if($relations){
+                    $relationships->$name = $relations ?? [];
+                }
             }
         }
         return $relationships;
