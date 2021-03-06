638459b260903c38f921b11033b05e2387f046ad
Hilkiah Makemo <HilkiahMakemo@PLANSTIN.LOCAL>
2021-03-06 07:45:16 -0700

    added orphan data logic

---
+++ src/Data/Scopes/OrphanScope.php
@@ -0,0 +1,130 @@
+<?php
+
+
+namespace Core\Data\Scopes;
+
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Database\Eloquent\Builder;
+
+class OrphanScope implements Scope
+{
+
+    /**
+     * All of the extensions to be added to the builder.
+     *
+     * @var array
+     */
+    protected $extensions = ['Orphaned', 'DeOrphaned', 'WithOrphaned', 'WithoutOrphaned', 'OnlyOrphaned'];
+
+    /**
+     * Apply the scope to a given Eloquent query builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @return void
+     */
+    public function apply(Builder $builder, Model $model)
+    {
+        $builder->whereNull($model->getQualifiedOrphanedAtColumn());
+    }
+
+    /**
+     * Extend the query builder with the needed functions.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @return void
+     */
+    public function extend(Builder $builder)
+    {
+        foreach ($this->extensions as $extension) {
+            $this->{"add{$extension}"}($builder);
+        }
+    }
+
+    /**
+     * Add the orphaned extension to the builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @return void
+     */
+    protected function addOrphaned(Builder $builder)
+    {
+        $builder->macro('orphan', function (Builder $builder) {
+            $builder->withoutOrphaned();
+
+            return $builder->update([$builder->getModel()->getOrphanedAtColumn() => null]);
+        });
+    }
+
+    /**
+     * Add the de-orphaned extension to the builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @return void
+     */
+    protected function addDeOrphaned(Builder $builder)
+    {
+        $builder->macro('deorphan', function (Builder $builder) {
+            $builder->withOrphaned();
+
+            return $builder->update([$builder->getModel()->getOrphanedAtColumn() => null]);
+        });
+    }
+
+    /**
+     * Add the with-orphaned extension to the builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @return void
+     */
+    protected function addWithOrphaned(Builder $builder)
+    {
+        $builder->macro('withOrphaned', function (Builder $builder, $withOrphaned = true) {
+            if (! $withOrphaned) {
+                return $builder->withoutOrphaned();
+            }
+
+            return $builder->withoutGlobalScope($this);
+        });
+    }
+
+    /**
+     * Add the without-orphaned extension to the builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @return void
+     */
+    protected function addWithoutOrphaned(Builder $builder)
+    {
+        $builder->macro('withoutOrphaned', function (Builder $builder) {
+            $model = $builder->getModel();
+
+            $builder->withoutGlobalScope($this)->whereNull(
+                $model->getQualifiedOrphanedAtColumn()
+            );
+
+            return $builder;
+        });
+    }
+
+    /**
+     * Add the only-orphaned extension to the builder.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $builder
+     * @return void
+     */
+    protected function addOnlyOrphaned(Builder $builder)
+    {
+        $builder->macro('onlyOrphaned', function (Builder $builder) {
+            $model = $builder->getModel();
+
+            $builder->withoutGlobalScope($this)->whereNotNull(
+                $model->getQualifiedOrphanedAtColumn()
+            );
+
+            return $builder;
+        });
+    }
+}
---
+++ src/Data/Services/OrphansRecord.php
@@ -0,0 +1,175 @@
+<?php
+
+namespace Core\Data\Services;
+
+use App\Scopes\OrphanScope;
+
+/**
+ * OrphansData
+ */
+trait OrphansRecord
+{
+
+    /**
+     * Indicates if the model is currently force deleting.
+     *
+     * @var bool
+     */
+    protected $forceDeleting = false;
+
+    /**
+     * Boot the orphaning trait for a model.
+     *
+     * @return void
+     */
+    public static function bootOrphansRecord()
+    {
+        static::addGlobalScope(new OrphanScope);
+    }
+    /*
+     * --------
+     *  EVENTS
+     * --------
+     */
+    /**
+     * Register a orphaning model event with the dispatcher.
+     *
+     * @param  \Closure|string  $callback
+     * @return void
+     */
+    public static function orphaning($callback)
+    {
+        static::registerModelEvent('orphaning', $callback);
+    }
+
+    /**
+     * Register a orphaned model event with the dispatcher.
+     *
+     * @param  \Closure|string  $callback
+     * @return void
+     */
+    public static function orphaned($callback)
+    {
+        static::registerModelEvent('orphaned', $callback);
+    }
+
+    /**
+     * Register a de-orphaning model event with the dispatcher.
+     *
+     * @param  \Closure|string  $callback
+     * @return void
+     */
+    public static function deOrphaning($callback)
+    {
+        static::registerModelEvent('deOrphaning', $callback);
+    }
+
+    /**
+     * Register a de-orphaned model event with the dispatcher.
+     *
+     * @param  \Closure|string  $callback
+     * @return void
+     */
+    public static function deOrphaned($callback)
+    {
+        static::registerModelEvent('deOrphaned', $callback);
+    }
+    /*
+     * --------
+     *  GETTERS
+     * --------
+     */
+    public function getOrphanedAtColumn()
+    {
+        return defined('static::ORPHANED_AT') ? static::ORPHANED_AT : 'orphaned_at';
+    }
+
+    public function getOrphanedAttribute()
+    {
+        return ! is_null($this->{$this->getOrphanedAtColumn()});
+    }
+
+    /**
+     * Get the fully qualified "orphaned at" column.
+     *
+     * @return string
+     */
+    public function getQualifiedOrphanedAtColumn()
+    {
+        return $this->qualifyColumn($this->getOrphanedAtColumn());
+    }
+
+    /*
+     * --------
+     *  ACTIONS
+     * --------
+     */
+    public function runOrphan($time = null)
+    {
+        $query = $this->setKeysForSaveQuery($this->newModelQuery());
+
+        $this->{$this->getOrphanedAtColumn()} = $time;
+
+        $columns = [$this->getOrphanedAtColumn() => $this->fromDateTime($time)];
+
+        if ($this->timestamps && ! is_null($this->getOrphanedAtColumn())) {
+            $this->{$this->getOrphanedAtColumn()} = $time;
+
+            $columns[$this->getOrphanedAtColumn()] = $this->fromDateTime($time);
+        }
+
+        $query->update($columns);
+
+        $this->syncOriginalAttributes(array_keys($columns));
+    }
+
+    protected function performOrphanOnModel()
+    {
+        if ($this->orphaning) {
+            $this->exists = false;
+
+            return $this->setKeysForSaveQuery($this->newModelQuery())->orphan();
+        }
+
+        return $this->runOrphan();
+    }
+
+
+    public function orphan()
+    {
+        $this->orphaning = true;
+
+        return tap($this->orphanData(), function ($orphaning) {
+            $this->orphaning = false;
+
+            if ($orphaning) {
+                $this->fireModelEvent('orphaning', false);
+            }
+        });
+    }
+
+    public function orphanData()
+    {
+        $time = $this->freshTimestamp();
+        return $this->runOrphanData($time);
+    }
+
+    public function deOrphan()
+    {
+        $this->deOrphaning = true;
+
+        return tap($this->deOrphanData(), function ($deOrphaning) {
+            $this->deOrphaning = false;
+
+            if ($deOrphaning) {
+                $this->fireModelEvent('deOrphaning', false);
+            }
+        });
+    }
+
+    public function deOrphanData()
+    {
+        return $this->runOrphanData();
+    }
+
+}
